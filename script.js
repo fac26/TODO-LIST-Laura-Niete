e3c3af80f2bfe65d9c55ea5a08e26949007202c6
Niete <nieteis@gmail.com>
2022-11-05 13:58:10 +0000

    2 Passed tests, need to write one for empty field

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-const form = document.getElementById("form");
-const input = document.getElementById("task-input");
-const taskTodo = document.getElementById("tasklist");
+//Selectors
+const taskInput = document.getElementById("task-input");
+const taskBtn = document.getElementById("addtaskbtn");
+const taskList = document.getElementById("tasklist");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  addTask();
-});
+//Event listeners
+taskBtn.addEventListener("click", addTask);
 
-function addTask(todo) {
-  let todoText = input.value;
+// ADD TASK
+function addTask(event) {
+  event.preventDefault(); //Prevents page from refreshing when button is clicked
 
-  if (todo) {
-    todoText = todo.text;
-  }
+  //Dynamically creating a new div for tasks and buttons to sit inside. This is what will be displayed when the button is clicked
+  const taskDiv = document.createElement("div");
+  taskDiv.classList.add("task");
+
+  //Create list items
+  const newTask = document.createElement("li");
+  newTask.innerText = taskInput.value;
+  newTask.classList.add("task-item");
+  taskDiv.appendChild(newTask);
+
+  //Check Mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  taskDiv.appendChild(completedButton);
+
+  //Delete button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  taskDiv.appendChild(trashButton);
+
+  //Append to UL
+  taskList.appendChild(taskDiv);
+
+  //Clear Input value
+  taskInput.value = "";
 }
--- script.test.js
+++ script.test.js
@@ -1,7 +1,31 @@
-const userInput = document.getElementById("task-input");
+const testInput = document.getElementById("task-input");
+const testBtn = document.getElementById("addtaskbtn");
+const testList = document.getElementById("tasklist");
+
+function createTestTask(task) {
+  testInput.value = task;
+  testBtn.click();
+  testInput.value = "";
+}
+
+// ADD tests
 
 test("Submitting a new task adds it to the list", () => {
-  userInput.value = "Example Task";
-  equal(userInput.value, "Example Task", "Task added successfully");
-  userInput.value = "";
+  createTestTask("Task1");
+  equal(testList.children.length, 1, "One task is added to the list");
+});
+
+test("Test to check input field is cleared after a task is added", () => {
+  createTestTask("Task1");
+  equal(testInput.value, "", "Input field is cleared after a task is added");
+});
+
+test("Test to see if there is an input within the input field", () => {
+  createTestTask("");
+  const error = document.querySelector("#errorMsg");
+  equal(
+    error.textContent,
+    "Please enter a task",
+    "Displayed error message for empty input"
+  );
 });
